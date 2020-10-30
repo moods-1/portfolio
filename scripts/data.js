95677382263c1511c397ed6ec26927880ff7257a
moods-1 <66252073+moods-1@users.noreply.github.com>
2020-10-30 14:11:44 -0400

    Weather app removed.

--- scripts/data.js
+++ scripts/data.js
@@ -1,21 +1,21 @@
-const data =[
+const data =[     
     {
         "id": 1,
+        "url": "https://covid-19-tracker-global.web.app",
+        "image": "/images/covidApp.png",
+        "gitLink":"https://github.com/moods-1/covid-19-tracker.git"
+    },
+    {
+        "id": 2,
         "url": "https://webbflixx.web.app",
         "image": "/images/webflixApp.png",
         "gitLink":"https://github.com/moods-1/webflix"
     },
     {
-        "id": 2,
+        "id": 3,
         "url": "https://trusting-chandrasekhar-9da4df.netlify.app/",
         "image": "/images/pizzeriaApp.png",
         "gitLink":"https://github.com/moods-1/pizzeria.git"
-    },
-    {
-        "id": 3,
-        "url": "./weatherApp/weatherApp.html",
-        "image": "/images/weatherApp.png",
-        "gitLink":"https://github.com/moods-1/portfolio/tree/master/weatherApp"
     }
-    
 ]
+    
