e21ab43cbdfd6411d8fcf21bfff75d07fbd0f2ca
moods-1 <66252073+moods-1@users.noreply.github.com>
2021-03-22 10:44:51 -0400

    Appearance overhaul.

--- scripts/app.js
+++ scripts/app.js
@@ -4,25 +4,25 @@ const navListItems = Array.from(
 );
 const projectImage = document.querySelectorAll(".image-container");
 const sendButton = document.getElementById("submit-btn");
-const burgerBox = document.querySelector('#burger-box');
+const burgerBox = document.querySelector("#burger-box");
 const food = document.querySelector("#burger-box");
 const foodImage = document.querySelector("#burger");
 const verticalNav = document.querySelector("#vertical-nav");
-const about = document.querySelector('.profile-container');
-const showcase = document.querySelector('#showcase');
-const contact = document.querySelector('#contact');
-const projectContainer = document.getElementById('project-container');
-const form = document.getElementById('contact-form');
+const about = document.querySelector(".profile-container");
+const showcase = document.querySelector("#showcase");
+const contact = document.querySelector("#contact");
+const projectContainer = document.getElementById("project-container");
+const form = document.getElementById("contact-form");
 let foodSource = "";
 let foodToggler = "";
 let counter = 0;
 
-const intervalTimer = () =>{
-  return{
-    start: ()=>{
-      mobileTimer = setInterval(()=>{
-        if(window.innerWidth < 670){
-          projectContainer.innerHTML =`
+const intervalTimer = () => {
+  return {
+    start: () => {
+      mobileTimer = setInterval(() => {
+        if (window.innerWidth < 670) {
+          projectContainer.innerHTML = `
             <div class="design-box" id="design${data[counter].id}">
               <div class="image-container">
                 <a href=${data[counter].url} rel="noopener noreferrer" target="_blank">
@@ -36,19 +36,19 @@ const intervalTimer = () =>{
               </div>
             </div>
             `;
-        if(counter < data.length) counter ++;
-        if(counter == data.length) counter = 0;
+          if (counter < data.length) counter++;
+          if (counter == data.length) counter = 0;
         }
       }, 2600);
     },
-    stop: ()=> clearInterval(mobileTimer) 
-  }
-}
+    stop: () => clearInterval(mobileTimer),
+  };
+};
 let timer = intervalTimer();
 timer.start();
 
-if(window.innerWidth < 670){
-  projectContainer.innerHTML +=`
+if (window.innerWidth < 670) {
+  projectContainer.innerHTML += `
     <div class="design-box" id="design${data[0].id}">
       <div class="image-container">
         <a href=${data[0].url} rel="noopener noreferrer" target="_blank">
@@ -64,9 +64,9 @@ if(window.innerWidth < 670){
     `;
 }
 
-const projectFiller = () =>{
-  data.forEach(box =>{
-    projectContainer.innerHTML +=`
+const projectFiller = () => {
+  data.forEach((box) => {
+    projectContainer.innerHTML += `
       <div class="design-box" id="design${box.id}">
         <div class="image-container">
           <a href=${box.url} rel="noopener noreferrer" target="_blank">
@@ -80,31 +80,31 @@ const projectFiller = () =>{
         </div>
       </div>
       `;
-  })
-}
+  });
+};
 
 // Done to prevent three design boxes coming down on page launch/refresh
 
-if(window.innerWidth < 670) timer.start();
+if (window.innerWidth < 670) timer.start();
 else projectFiller();
 
-// Required for when the setInterval is not actively altering the DOM 
+// Required for when the setInterval is not actively altering the DOM
 // when the screen size is greater than 669px. Only one designBox is
 // present in the projectContainer when the screen size goes back above
 // 669px.
 
-screenTracker = ()=>{
-  if(window.innerWidth >= 670){
-    if(projectContainer.childElementCount < 2){
+screenTracker = () => {
+  if (window.innerWidth >= 670) {
+    if (projectContainer.childElementCount < 2) {
       projectContainer.innerHTML = "";
       projectFiller();
-    } 
+    }
   }
-}
+};
 
 burgerBox.addEventListener("click", () => {
   if (foodToggler === 0) {
-      navListItems.reverse().forEach((link, index) => {
+    navListItems.reverse().forEach((link, index) => {
       link.style.animation = `navEaseOut 0.5s ease-in-out forwards ${
         index / 7
       }s`;
@@ -113,7 +113,7 @@ burgerBox.addEventListener("click", () => {
     setTimeout(() => {
       foodToggler = 1;
       verticalNav.classList.add("d-none");
-    }, 700)
+    }, 700);
   } else {
     foodSource = "/images/fries.png";
     foodToggler = 0;
@@ -123,14 +123,14 @@ burgerBox.addEventListener("click", () => {
         (link.style.animation = `navEaseIn 0.5s ease-in-out forwards ${
           index / 7 + 0.2
         }s`)
-    )
+    );
   }
   foodImage.setAttribute("src", foodSource);
 });
 
 // Email
 
-form.addEventListener("submit", e =>{
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   window.location.href = "/contact.php";
-})
+});
